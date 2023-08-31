6142dff653298def928e958038e0bb871194eb1e
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2023-08-31 04:35:36 +0100

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// cannister code goes here
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
+import { v4 as uuidv4 } from 'uuid';
+type Patient = Record<{
+
+}>
